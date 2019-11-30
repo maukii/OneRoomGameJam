92d87b65c7e95508049aac3f67fdf50f92a63e28
Kiiskinen Mauno TTK17SP <maunokiiskinen@kamk.fi>
2019-11-30 16:25:24 +0200

    Add doge prefab with physics tail

--- OneRoom/Assets/Scripts/Doge.cs
+++ OneRoom/Assets/Scripts/Doge.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class Doge : MonoBehaviour
 {
 
-    
+    [SerializeField] private float movementSpeed = 5.0f;
+    private Rigidbody rb;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.MovePosition(transform.position + transform.forward * movementSpeed * Time.deltaTime);
+        }
+    }
 
 }
