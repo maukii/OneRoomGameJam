1b06292e03682f8c673236ef7d48e7f1668a25d6
Kiiskinen Mauno TTK17SP <maunokiiskinen@kamk.fi>
2019-11-30 18:03:33 +0200

    Doge updated animator and script

--- OneRoom/Assets/Scripts/Doge.cs
+++ OneRoom/Assets/Scripts/Doge.cs
@@ -7,11 +7,12 @@ public class Doge : MonoBehaviour
 
     [SerializeField] private float movementSpeed = 5.0f;
     private Rigidbody rb;
-
+    private Animator anim;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -19,6 +20,11 @@ public class Doge : MonoBehaviour
         if(Input.GetKey(KeyCode.A))
         {
             rb.MovePosition(transform.position + transform.forward * movementSpeed * Time.deltaTime);
+            anim.SetBool("moving", true);
+        }
+        else
+        {
+            anim.SetBool("moving", false);
         }
     }
 
